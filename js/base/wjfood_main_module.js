f6a9f63eb275eac3aa3046ae2721ca48a1de5566
landa000 <landaup000@gmail.com>
2020-11-21 10:09:21 +0900

    first upload

---
+++ js/base/wjfood_main_module.js
@@ -0,0 +1,12 @@
+// wjfood_main_module.js
+
+(function($){
+
+  var headBox = $('#headBox');
+
+  var footBox = $('#footBox');
+
+  headBox.load('./module/headBox.html');
+  footBox.load('./module/footBox.html');
+
+})(jQuery);
---
+++ js/src/main/headBox.js
@@ -0,0 +1,1 @@
+// wjfood > headBox.js
