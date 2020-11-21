4fafd7c07460830dfea57aa93e90927cbd3c640d
landa000 <landaup000@gmail.com>
2020-11-21 17:43:43 +0900

    201121 header add

--- js/base/wjfood_main_module.js
+++ js/base/wjfood_main_module.js
@@ -3,10 +3,12 @@
 (function($){
 
   var headBox = $('#headBox');
+  var viewBox = $('#viewBox');
 
   var footBox = $('#footBox');
 
   headBox.load('./module/headBox.html');
+  viewBox.load('./main/viewBox.html');
   footBox.load('./module/footBox.html');
 
 })(jQuery);
