42feda6ba6156e695f8542ced24b8fdcada50b91
landa000 <landaup000@gmail.com>
2020-12-23 18:03:36 +0900

    notice main+iframe

--- js/base/wjfood_main_module.js
+++ js/base/wjfood_main_module.js
@@ -4,12 +4,14 @@
 
   var headBox = $('#headBox');
   var viewBox = $('#viewBox');
+  var newsBox = $('#newsBox');
   var brandBox = $('#brandBox');
   var submenuBox = $('#submenuBox');
   var footBox = $('#footBox');
 
   headBox.load('./module/headBox.html');
   viewBox.load('./main/viewBox.html');
+  newsBox.load('./main/newsBox.html');
   brandBox.load('./main/brandBox.html');
   submenuBox.load('./main/submenuBox.html');
   footBox.load('./module/footBox.html');
---
+++ js/src/main/newsBox.js
@@ -0,0 +1,1 @@
+// newsBox.js
